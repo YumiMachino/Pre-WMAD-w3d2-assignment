62d8f8810b2e03621df71eeb41f9e549ed86cd3b
YumiMachino <y.y.1515m@gmail.com>
2020-08-19 03:00:45 +0900

    update on function7.js

--- function7.js
+++ function7.js
@@ -1,10 +1,8 @@
 // write a function that joins the array items together into one string, and log it to the console
-let brokenSentence = ['I', 'am', 'a', 'broken', 'sentence'];
+let brokenSentence = ["I", "am", "a", "broken", "sentence"];
 
-
-
-const joinSentence= function () {
-
+const joinSentence = function(brokenSentence) {
+  console.log(brokenSentence.join(" "));
 };
 
 joinSentence(brokenSentence);
