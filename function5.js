ec0094cb7a245407bb9f0224aa0545077751a5d5
YumiMachino <y.y.1515m@gmail.com>
2020-08-19 02:39:32 +0900

    update on function5.js

--- function5.js
+++ function5.js
@@ -1,10 +1,11 @@
 // write a function that takes every item in the array, and adds the word 'HOORAY' to the end of it
 
-let food = ['apple', 'orange', 'cake', 'carrots', 'pasta', 'rice', 'eggs'];
+let food = ["apple", "orange", "cake", "carrots", "pasta", "rice", "eggs"];
 
-const addHooray = function () {
-
+const addHooray = function() {
+  for (let i = 0; i < food.length; i++) {
+    console.log(food[i] + " " + "HOORAY");
+  }
 };
 
 addHooray(food);
-
