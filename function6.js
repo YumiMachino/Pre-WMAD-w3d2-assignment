31f704093cd9cb13310f496d2a9cc2519415931f
YumiMachino <y.y.1515m@gmail.com>
2020-08-19 04:19:59 +0900

    updated on function6.js

--- function6.js
+++ function6.js
@@ -4,11 +4,12 @@ let food = ["apple", "orange", "cake", "carrots", "pasta", "rice", "eggs"];
 let numbers = ["1", "2", "3", "4"];
 
 const addTheLast = function(food, numbers) {
-  //   console.log(food[0, 1, 2, 3, 4, 5, 6] + " " + numbers[numbers.length - 1]);
-  for (let i = 0; i < food.length; i++) {
-    console.log('"' + food[i] + '"');
-  }
-  console.log('"' + numbers[numbers.length - 1] + '"');
+  //   for (let i = 0; i < food.length; i++) {
+  //     console.log('"' + food[i] + '"');
+  //   }
+  //   console.log('"' + numbers[numbers.length - 1] + '"');
+  food.push(numbers[numbers.length - 1]);
+  console.log(food);
 };
 
 addTheLast(food, numbers);
