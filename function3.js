fc95f81b2ae8c8131852f2bbe5bd249a9fd522db
YumiMachino <y.y.1515m@gmail.com>
2020-08-19 02:12:18 +0900

    update on function3.js

--- function3.js
+++ function3.js
@@ -1,10 +1,11 @@
 // write a function that console logs each item of the array one line at a time
 
-let example = [1, 2, 3, 4, 'five', 6, 7];
+let example = [1, 2, 3, 4, "five", 6, 7];
 
-const logItems = function () {
-
+const logItems = function() {
+  for (i = 0; i < example.length; i++) {
+    console.log(example[i]);
+  }
 };
 
-logitems(example);
-
+logItems(example);
